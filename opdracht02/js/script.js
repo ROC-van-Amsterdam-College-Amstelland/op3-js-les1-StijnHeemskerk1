e7b4784f462d34577b005797942fa09f6c2a5a02
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-02-11 15:10:10 +0100

    opdracht2 af

--- opdracht02/js/script.js
+++ opdracht02/js/script.js
@@ -4,8 +4,11 @@ var go = "groen";
 
 window.onload = function(){
     document.getElementById("light-top").innerHTML = stop;
+    document.getElementById("light-top").style.backgroundColor='red';
     document.getElementById("light-middle").innerHTML = maybe;
-    document.getElementById("light-bottom").innerHTML = go;
+    document.getElementById("light-middle").style.backgroundColor='orange';
+    document.getElementById("light-bottom").innerHTML = 'go';
+    document.getElementById("light-bottom").style.backgroundColor='green';
 
     //JOUW CODE HIER
 }
